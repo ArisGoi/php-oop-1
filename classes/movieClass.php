57e0ee97766c2d0de4c05e596abbae40edae4657
ArisGoi <aris.goi97@gmail.com>
2021-11-02 16:08:53 +0100

    commit

---
+++ classes/movieClass.php
@@ -0,0 +1,50 @@
+<?php
+
+// Definire una classe Movie
+class Movie{
+    // dichiarare variabili d'istanza
+    private $titolo;
+    private $regista;
+    private $anno;
+    private $durata;
+    private $lingua;
+    private $prezzo;
+
+    // definire un costruttore
+    function __construct($_titolo, $_regista, $_anno, $_durata, $_lingua, $_prezzo)
+    {
+        $this->titolo = $_titolo;
+        $this->regista = $_regista;
+        $this->anno = $_anno;
+        $this->durata = $_durata;
+        $this->lingua = $_lingua;
+        $this->prezzo = $_prezzo;
+    }
+
+    // definire almeno un metodo
+    function getPrezzo($userEta){
+        if($userEta < 20){
+            $prezzoFinale = $this->prezzo - ($this->prezzo * 0.50);
+            return number_format($prezzoFinale, 2, '.', ',');
+        } else if ($userEta < 60){
+            $prezzoFinale = $this->prezzo - ($this->prezzo * 0.25);
+            return number_format($prezzoFinale, 2, '.', ',');
+        } else {
+            $prezzoFinale = $this->prezzo;
+            return number_format($prezzoFinale, 2, '.', ',');
+        }
+    }
+
+    function getInfo($what){
+        $info = [
+            "titolo" => $this->titolo,
+            "regista" => $this->regista,
+            "anno" => $this->anno,
+            "durata" => $this->durata,
+            "lingua" => $this->lingua,
+        ];
+        return $info[$what];
+    }
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+require __DIR__ . "./classes/movieClass.php";
+
+// vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+$f_avatar = new Movie("Avatar", "James Cameron", 2009, 162, "inglese", 9.99);
+$f_avengers_endgame = new Movie ("Avengers: Endgame", "Anthony e Joe Russo", 2019, 181, "inglese", 13.99);
+
+// var_dump($f_avatar->getInfo()); die;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        p{margin: 0;}
+        h2{margin-bottom: 0;}
+    </style>
+</head>
+<body>
+    <ul>
+        <li>
+            <h2><?= $f_avatar->getInfo("titolo") ?></h2>
+            <p><?= $f_avatar->getInfo("anno") ?></p>
+            <p>durata: <?= $f_avatar->getInfo("durata") ?></p>
+            <p>lingua: <?= $f_avatar->getInfo("lingua") ?></p>
+            <p>prezzo: <?= $f_avatar->getPrezzo(18) ?></p>
+        </li>
+        <li>
+            <h2><?= $f_avengers_endgame->getInfo("titolo") ?></h2>
+            <p><?= $f_avengers_endgame->getInfo("anno") ?></p>
+            <p>durata: <?= $f_avengers_endgame->getInfo("durata") ?></p>
+            <p>lingua: <?= $f_avengers_endgame->getInfo("lingua") ?></p>
+            <p>prezzo: <?= $f_avengers_endgame->getPrezzo(18) ?></p>
+        </li>
+    </ul>
+</body>
+</html>
